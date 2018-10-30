510da994b5a10327d59820fa06f6eed151f5fb2e
Administrator <952280669@qq.com>
2018-10-30 18:44:11 +0800

    增加log类

--- logs/log.py
+++ logs/log.py
@@ -1,6 +1,5 @@
 # __author__ = 'Yang'
 # -*- coding:utf-8 -*-
-
 import os
 import logging
 from logging.handlers import TimedRotatingFileHandler
@@ -10,8 +9,17 @@ from logging.handlers import TimedRotatingFileHandler
 需导入 logger 模块
 """
 
+
 class Logger(object):
-    def __init__(self, logger_name='framework'):
+    def __init__(self, logger_name='frameWork'):
         self.logger = logging.getLogger(logger_name)
         logging.root.setLevel(logging.NOTSET)
-        c = config
+        self.log_file_name = 'test.log'
+        self.backup_count = 5
+        # 日志输出的级别
+        self.console_output_level = 'WARNING'
+        self.file_output_lever = 'DEBUG'
+        # 日志输出格式
+        self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    def get_logger(self):
